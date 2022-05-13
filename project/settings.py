8cd0f79c8e2a40aed39d488f9c4299c57eb4d8b6
redbor24 <redbor24@gmail.com>
2022-05-13 13:57:09 +0300

    Убраны значения по умолчанию про считывании некоторых переменных окружения.

--- project/settings.py
+++ project/settings.py
@@ -8,11 +8,11 @@ env.read_env()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': env.str('DBHOST', ''),
-        'PORT': env.str('DBPORT', ''),
-        'NAME': env.str('DBNAME', ''),
-        'USER': env.str('DBUSER', ''),
-        'PASSWORD': env.str('DBPASS', ''),
+        'HOST': env.str('DBHOST'),
+        'PORT': env.str('DBPORT'),
+        'NAME': env.str('DBNAME'),
+        'USER': env.str('DBUSER'),
+        'PASSWORD': env.str('DBPASS'),
     }
 }
 
